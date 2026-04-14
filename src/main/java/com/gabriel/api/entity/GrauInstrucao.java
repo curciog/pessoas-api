6c736ee2f90531b37d14d2d7afa2e4ede80bf9de
curciog <curciogdev@gmail.com>
2026-04-14 15:02:10 -0300

    refactor: add equals and hashCode baseado no id em GrauInstrucao

--- src/main/java/com/gabriel/api/entity/GrauInstrucao.java
+++ src/main/java/com/gabriel/api/entity/GrauInstrucao.java
@@ -1,18 +1,27 @@
 package com.gabriel.api.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "graus_instrucao")
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class GrauInstrucao { // ajuste pro code review
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class GrauInstrucao {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
 
     @Column(nullable = false, unique = true)
