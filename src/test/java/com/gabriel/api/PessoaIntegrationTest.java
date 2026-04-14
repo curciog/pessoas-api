21315eceab09bd646c72f63cd891bc991bd28c47
curciog <curciogdev@gmail.com>
2026-04-14 16:20:07 -0300

    test: utiliza JSON direto nos testes de integração de Pessoa

--- src/test/java/com/gabriel/api/PessoaIntegrationTest.java
+++ src/test/java/com/gabriel/api/PessoaIntegrationTest.java
@@ -26,44 +26,45 @@ public class PessoaIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper objectMapper;
-
     @Test
     void deveCriarPessoaComSucesso() throws Exception {
-        PessoaRequest request = new PessoaRequest(
-                "Teste JUnit",
-                "00005555556",
-                "1234567",
-                1L,
-                1L
-        );
+        String json = """
+                {
+                "nome": "Teste JUnit",
+                "cpf": "00005555556",
+                "rg": "1234567",
+                "estadoCivilId": 1,
+                "grauInstrucaoId": 1
+                }
+                """;
 
         mockMvc.perform(post("/pessoas")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(request)))
+                .content(json))
                 .andExpect(status().isCreated())
                 .andExpect(content().string(containsString("Teste JUnit")));
     }
 
     @Test
     void naoDevePermitirCpfDuplicado() throws Exception {
-        PessoaRequest request = new PessoaRequest(
-                "Pessoa Duplicada",
-                "00005555557",
-                "7654321",
-                1L,
-                1L
-        );
+        String json = """
+                {
+                "nome": "Pessoa Duplicada",
+                "cpf": "00005555557",
+                "rg": "7654321",
+                "estadoCivilId": 1,
+                "grauInstrucaoId": 1
+                }
+                """;
 
         mockMvc.perform(post("/pessoas")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(request)))
+                .content(json))
                 .andExpect(status().isCreated());
 
         mockMvc.perform(post("/pessoas")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(request)))
+                .content(json))
                 .andExpect(status().isConflict())
                 .andExpect(content().string(containsString("Já existe uma pessoa cadastrada com esse CPF.")));
     }
