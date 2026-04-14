80318f42a7897af0caa08636e155aff7977e90ae
curciog <curciogdev@gmail.com>
2026-04-14 16:47:24 -0300

    fix: corrige PessoaRequest após bug de merge

--- src/main/java/com/gabriel/api/dto/PessoaRequest.java
+++ src/main/java/com/gabriel/api/dto/PessoaRequest.java
@@ -2,63 +2,22 @@ package com.gabriel.api.dto;
 
 import jakarta.validation.constraints.NotNull;
 
-public class PessoaRequest {
+public record PessoaRequest(
 
-    private String nome;
-    private String cpf;
-    private String rg;
-    private Long estadoCivilId;
-    private Long grauInstrucaoId;
+        @NotNull(message = "O nome é obrigatório.")
+        String nome,
 
-    public PessoaRequest() {
-    }
+        @NotNull(message = "O CPF é obrigatório.")
+        String cpf,
 
-    public PessoaRequest(String nome, String cpf, String rg, Long estadoCivilId, Long grauInstrucaoId) {
-        this.nome = nome;
-        this.cpf = cpf;
-        this.rg = rg;
-        this.estadoCivilId = estadoCivilId;
-        this.grauInstrucaoId = grauInstrucaoId;
-    }
+        @NotNull(message = "O RG é obrigatório.")
+        String rg,
 
-    @NotNull(message = "O nome é obrigatório.")
-    public String getNome() {
-        return nome;
-    }
+        @NotNull(message = "O estado civil é obrigatório.")
+        Long estadoCivilId,
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+        @NotNull(message = "O grau de instrução é obrigatório.")
+        Long grauInstrucaoId
 
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getRg() {
-        return rg;
-    }
-
-    public void setRg(String rg) {
-        this.rg = rg;
-    }
-
-    public Long getEstadoCivilId() {
-        return estadoCivilId;
-    }
-
-    public void setEstadoCivilId(Long estadoCivilId) {
-        this.estadoCivilId = estadoCivilId;
-    }
-
-    public Long getGrauInstrucaoId() {
-        return grauInstrucaoId;
-    }
-
-    public void setGrauInstrucaoId(Long grauInstrucaoId) {
-        this.grauInstrucaoId = grauInstrucaoId;
-    }
+) {
 }
