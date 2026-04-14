52055c7172246a08ea5544da271a83a270d13a3d
curciog <curciogdev@gmail.com>
2026-04-14 14:57:01 -0300

    refactor: add equals and hashCode baseado no id em EstadoCivil

--- src/main/java/com/gabriel/api/dto/PessoaRequest.java
+++ src/main/java/com/gabriel/api/dto/PessoaRequest.java
@@ -1,10 +1,6 @@
 package com.gabriel.api.dto;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 public record PessoaRequest(
 
@@ -24,19 +20,3 @@ public record PessoaRequest(
         Long grauInstrucaoId
 ) {
 }
-
-/*@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class PessoaRequest {
-
-    @NotNull(message = "O nome é obrigatório.")
-    private String nome;
-
-    private String cpf;
-    private String rg;
-
-    private Long estadoCivilId;
-    private Long grauInstrucaoId;
-}*/
